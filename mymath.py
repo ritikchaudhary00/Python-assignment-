8b228b9a77ebf5a2d5cc3a7995e9d91bdcd84582
ritikchaudhary00 <ritikcdry900@gmail.com>
2025-07-03 16:59:00 +0545

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial not defined for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        for i in range(2, n + 1):
+            fact *= i
+        return fact
+
+
